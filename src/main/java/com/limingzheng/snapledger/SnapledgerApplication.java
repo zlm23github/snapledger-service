23f2751bcc67bf1275e76026e67b6e9517e2bcf5
liming <zlm17unl@gmail.com>
2025-06-29 22:39:27 -0400

    init java springboot

---
+++ src/main/java/com/limingzheng/snapledger/SnapledgerApplication.java
@@ -0,0 +1,13 @@
+package com.limingzheng.snapledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnapledgerApplication.class, args);
+	}
+
+}
